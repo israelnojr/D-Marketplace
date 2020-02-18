bc29707300e5be6ebfffcd1ee1dc94aefabc5d59
israelnojr <israeliloba@gmail.com>
2020-02-18 02:01:29 +0100

    contract validation, testing, and deployment

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -8,16 +8,27 @@ contract Marketplace {
     struct Product {
         uint id;
         string name;
+        string desc;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
     event ProductCreated(
         uint id,
         string name,
+        string desc,
         uint price,
-        address owner,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased(
+        uint id,
+        string name,
+        string desc,
+        uint price,
+        address payable owner,
         bool purchased
     );
 
@@ -25,9 +36,10 @@ contract Marketplace {
         name = "Juis Decentralized MarketPlace";
     }
 
-    function createProduct(string memory _name, uint _price) public {
+    function createProduct(string memory _name, string memory _desc, uint _price) public {
         //Validate
-        require(bytes(name).length > 0);
+        require(bytes(_name).length > 0);
+        require(bytes(_desc).length > 10);
         require(_price > 0);
 
         //increment product count
@@ -37,6 +49,7 @@ contract Marketplace {
         products[productCount] = Product(
             productCount,
             _name,
+            _desc,
             _price,
             msg.sender,
             false
@@ -46,9 +59,42 @@ contract Marketplace {
         emit ProductCreated(
             productCount,
             _name,
+            _desc,
             _price,
             msg.sender,
             false
+        );
+    }
+
+    function purchaseProduct(uint _id) public payable {
+        // fetch product
+        Product memory _product = products[_id];
+        // fetch owner
+        address payable _seller = _product.owner;
+        // make sure product_id is valid
+        require(_product.id > 0 && _product.id <= productCount);
+        // check if buyer has enough eth
+        require(msg.value >= _product.price);
+        // check if product is not already purchased
+        require(!_product.purchased);
+        //check that seller is not buyer
+        require(_seller != msg.sender);
+        // purchase it
+        _product.owner = msg.sender; 
+        // mark as purchased
+        _product.purchased = true;
+        //update product
+        products[_id] = _product;
+        //pay seller
+        address(_seller).transfer(msg.value);
+        //trigger event
+        emit ProductPurchased(
+            productCount,
+            _product.name,
+            _product.desc,
+            _product.price,
+            msg.sender,
+            true
         );
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,4 +1,7 @@
 const Marketplace = artifacts.require('./Marketplace.sol');
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
 
 contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace 
@@ -24,17 +27,86 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
         let result, productCount
 
         before(async () => {
-            result = await marketplace.createProduct('Iphone X', web3.utils.toWei('1', 'Ether'),{from: seller})
+            result = await marketplace.createProduct(
+            'Iphone X',
+            'An Iphone X from Apple Inc. 32GB in good condition',
+            web3.utils.toWei('1', 'Ether'),{from: seller})
             productCount = await marketplace.productCount()
         })
+        // test if product is created with proper peremeters
         it('Create products', async () => {
             assert.equal(productCount, 1)
             const event = result.logs[0].args
             assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
             assert.equal(event.name, 'Iphone X' , ' Name is correct')
+            assert.equal(event.desc, 'An Iphone X from Apple Inc. 32GB in good condition', ' Description is correct')
             assert.equal(event.price, '1000000000000000000' , 'Price is correct')
             assert.equal(event.owner, seller, 'Owner is correct')
             assert.equal(event.purchased, false, 'Purchased is correct')
+
+            // checking for failure: Product must have a name
+           await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected;
+           // checking for failure: Product must have a Price
+           await await marketplace.createProduct('Iphone X', 'An Iphone X from Apple Inc. 32GB in good condition', 0, {from: seller}).should.be.rejected;
+           // checking for failure: Product must have a description
+           await await marketplace.createProduct('Iphone X', '', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected;
         })
+
+        // check products are available 
+        it('lists products', async () => {
+            const product = await marketplace.products(productCount)
+            assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(product.name, 'Iphone X' , ' Name is correct')
+            assert.equal(product.desc, 'An Iphone X from Apple Inc. 32GB in good condition' , ' Description is correct')
+            assert.equal(product.price, '1000000000000000000' , 'Price is correct')
+            assert.equal(product.owner, seller, 'Owner is correct')
+            assert.equal(product.purchased, false, 'Purchased is correct')
+        })
+
+        it('sells products', async () => {
+            // Track seller balance 
+            let oldSellerBalance 
+            oldSellerBalance = await web3.eth.getBalance(seller)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+            //buyer makes purchase
+            result = await marketplace.purchaseProduct(productCount, {
+                from: buyer,
+                value: web3.utils.toWei('1', 'Ether')
+            })
+
+            // check logs
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'Iphone X' , ' Name is correct')
+            assert.equal(event.desc, 'An Iphone X from Apple Inc. 32GB in good condition' , ' Description is correct')
+            assert.equal(event.price, '1000000000000000000' , 'Price is correct')
+            assert.equal(event.owner, buyer, 'Owner is correct')
+            assert.equal(event.purchased, true, 'Purchased is correct')
+
+            // // check seller recieved fund
+            let newSellerBalance 
+            newSellerBalance = await web3.eth.getBalance(seller)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+            let price
+            price = web3.utils.toWei('1', 'Ether')
+            price = new web3.utils.BN(price)
+            // set the expected balance for seller
+            const expectedBal = oldSellerBalance.add(price)
+            // if seller new and expected balace is equal
+            assert.equal(newSellerBalance.toString(), expectedBal.toString())
+            
+            //check for failures :
+            
+            //buy none exist product
+            await marketplace.purchaseProduct(99, {from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+            // buy product with insufficient balance
+            await marketplace.purchaseProduct(productCount, {from: buyer, value: web3.utils.toWei('0.01', 'Ether')}).should.be.rejected;
+            // fail if deployer tries to buy
+            await marketplace.purchaseProduct(productCount, {from: deployer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+            // buyer can't buy twice
+            await marketplace.purchaseProduct(productCount, {from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+        })
+
     })
 })
